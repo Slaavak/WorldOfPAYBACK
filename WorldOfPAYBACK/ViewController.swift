86b020eff56ea96754620f5dedc4c07e6e0ac780
Slava Korolevich <ext_vkorolevich@rendcredit.ru>
2022-10-24 13:52:26 +0300

    Initial Commit

---
+++ WorldOfPAYBACK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorldOfPAYBACK
+//
+//  Created by Slava Korolevich on 24.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
